83c35450e4948e25e89f38286b613c685bf4b346
Athrva Bhawsar <athrvabh124@gmail.com>
2025-07-24 22:02:05 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Med Chatbot",
+    version="0.1.0",
+    author="Atharv",
+    author_email="athrvabh124@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
